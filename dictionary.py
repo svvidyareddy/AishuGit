c97ab7bd46e7b87ad598ab898fef18a033355a8b
svvidyareddy <svvidyareddy@gmail.com>
2024-08-02 22:25:02 -0700

    first commit

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+#DICTIONARY
+car={"Brand" : "Ford",
+    "Model" : "Mustag",
+    "Year":1964}
+for key in car:
+    print("key:" +key+ "," + "values:",str+(car([key])))
---
+++ exp10.py
@@ -0,0 +1,3 @@
+#exp-10
+odd_square={x:x*x for x in range(11) if x%2--1}
+print("The suare of ood number is:",odd_square)
